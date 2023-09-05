771c7db30c4e9779eeab91f13fa19698e4bd9a02
schurgan <51407784+schurgan@users.noreply.github.com>
2023-09-05 21:16:04 +0200

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -28,7 +28,7 @@
 <plugin key="MELCloud" version="0.7.8" name="MELCloud plugin" author="gysmo" wikilink="http://www.domoticz.com/wiki/Plugins/MELCloud.html" externallink="http://www.melcloud.com">
     <params>
         <param field="Username" label="Email" width="200px" required="true" />
-        <param field="Password" label="Password" width="200px" required="true" />
+        <param field="Password" label="Password" width="200px" required="true" password="true"/>
         <param field="Mode1" label="GMT Offset" width="75 px">
             <options>
                 <option label="-12" value="-12"/>
@@ -112,7 +112,7 @@ class BasePlugin:
 
     domoticz_levels = {}
     domoticz_levels["mode"] = {"0": 0, "10": 1, "20": 3, "30": 7, "40": 2, "50": 8}
-    domoticz_levels["mode_pic"] = {"0": 9, "10": 15, "20": 16, "30": 7, "40": 11}
+    domoticz_levels["mode_pic"] = {"0": 9, "10": 15, "20": 16, "30": 7, "40": 11, "50": 10}
     domoticz_levels["fan"] = {"0": 1, "10": 2, "20": 3, "30": 4, "40": 255, "50": 0, "60": 1}
     domoticz_levels["temp"] = {"0": 16, "10": 17, "20": 18, "30": 19, "40": 20, "50": 21,
                                "60": 22, "70": 23, "80": 24, "90": 25, "100": 26,
@@ -129,8 +129,10 @@ class BasePlugin:
     def onStart(self):
         Domoticz.Heartbeat(25)
         if Parameters["Mode6"] == "Debug":
-            Domoticz.Debugging(62)
+            Domoticz.Debugging(-1)
         # Start connection to MELCloud
+        #Domoticz.Debugging(62)
+        #Domoticz.Debugging(-1)
         self.melcloud_conn = Domoticz.Connection(Name="MELCloud", Transport="TCP/IP",
                                                  Protocol="HTTPS", Address=self.melcloud_baseurl,
                                                  Port=self.melcloud_port)
@@ -324,7 +326,7 @@ class BasePlugin:
                 Devices[1+current_unit['idoffset']].Update(nValue=1, sValue=str(Level), Image=11)
             elif Level == 50:
                 Domoticz.Log("Set to Auto the unit "+current_unit['name'])
-                Devices[1+current_unit['idoffset']].Update(nValue=1, sValue=str(Level), Image=11)
+                Devices[1+current_unit['idoffset']].Update(nValue=1, sValue=str(Level), Image=10)
             if Level != 0:
                 flag = 1
                 current_unit['power'] = 'true'
@@ -345,7 +347,7 @@ class BasePlugin:
         elif switch_type == 'Fan':
             flag = 8
             current_unit['set_fan'] = self.domoticz_levels['fan'][str(Level)]
-            Domoticz.Log("Change FAN  to value {0} for {1} ".format(self.domoticz_levels['temp'][str(Level)], current_unit['name']))
+            Domoticz.Log("Change FAN  to value {0} for {1} ".format(self.domoticz_levels['fan'][str(Level)], current_unit['name']))
             Devices[Unit].Update(nValue=Devices[Unit].nValue, sValue=str(Level))
         elif switch_type == 'Temp':
             flag = 4
@@ -388,9 +390,9 @@ class BasePlugin:
         else:
             self.runAgain = self.runAgain - 1
             if self.runAgain <= 0:
-                if self.melcloud_conn is None:
-                    self.melcloud_conn = Domoticz.Connection(Name="MELCloud", Transport="TCP/IP", Protocol="HTTPS",
-                                                             Address=self.melcloud_baseurl, Port=self.melcloud_port)
+                self.melcloud_conn = Domoticz.Connection(Name="MELCloud", Transport="TCP/IP", Protocol="HTTPS",
+                                                         Address=self.melcloud_baseurl, Port=self.melcloud_port)
+                self.melcloud_key = None
                 self.melcloud_conn.Connect()
                 self.runAgain = 6
             else:
@@ -432,6 +434,24 @@ class BasePlugin:
             self.melcloud_conn.Send({'Verb': 'POST', 'URL': url, 'Headers': headers, 'Data': values})
         return True
 
+    def melcloud_send_data_json(self, url, values, state):
+        self.melcloud_state = state
+        if self.melcloud_key is not None:
+            headers = {'Content-Type': 'application/json;',
+                       'Host': self.melcloud_baseurl,
+                       'User-Agent': 'Domoticz/1.0',
+                       'X-MitsContextKey': self.melcloud_key}
+            if state == "SET":
+                self.melcloud_conn.Send({'Verb': 'POST', 'URL': url, 'Headers': headers, 'Data': values})
+            else:
+                self.melcloud_conn.Send({'Verb': 'GET', 'URL': url, 'Headers': headers, 'Data': values})
+        else:
+            headers = {'Content-Type': 'application/x-www-form-urlencoded;',
+                       'Host': self.melcloud_baseurl,
+                       'User-Agent': 'Domoticz/1.0'}
+            self.melcloud_conn.Send({'Verb': 'POST', 'URL': url, 'Headers': headers, 'Data': values})
+        return True
+
     def melcloud_login(self):
         data = "AppVersion=1.9.3.0&Email={0}&Password={1}".format(Parameters["Username"], Parameters["Password"])
         self.melcloud_send_data(self.melcloud_urls["login"], data, "LOGIN")
@@ -459,11 +479,17 @@ class BasePlugin:
         self.melcloud_send_data(self.melcloud_urls["list_unit"], None, "UNITS_INIT")
         return True
 
-    def melcloud_set(self, unit, flag):
-        post_fields = "Power={0}&DeviceID={1}&OperationMode={2}&SetTemperature={3}&SetFanSpeed={4}&VaneHorizontal={5}&VaneVertical={6}&EffectiveFlags={7}&HasPendingCommand=true"
-        post_fields = post_fields.format(unit['power'], unit['id'], unit['op_mode'], unit['set_temp'], unit['set_fan'], unit['vaneH'], unit['vaneV'], flag)
+    def melcloud_set_urlencode(self, unit, flag):
+        post_fields = 'Power={0}&DeviceID={1}&OperationMode={2}&SetTemperature={3}&SetFanSpeed={4}&VaneHorizontal={5}&VaneVertical={6}&EffectiveFlags={7}&HasPendingCommand=true'
+        post_fields = post_fields.format(str(unit['power']).lower(), unit['id'], unit['op_mode'], unit['set_temp'], unit['set_fan'], unit['vaneH'], unit['vaneV'], flag)
         Domoticz.Debug("SET COMMAND SEND {0}".format(post_fields))
         self.melcloud_send_data(self.melcloud_urls["set_unit"], post_fields, "SET")
+
+    def melcloud_set(self, unit, flag):
+        post_fields = "'Power':{0},'DeviceID':{1},'OperationMode':{2},'SetTemperature':{3},'SetFanSpeed':{4},'VaneHorizontal':{5},'VaneVertical':{6},'EffectiveFlags':{7},'HasPendingCommand':true"
+        post_fields = post_fields.format(str(unit['power']).lower(), unit['id'], unit['op_mode'], unit['set_temp'], unit['set_fan'], unit['vaneH'], unit['vaneV'], flag)
+        Domoticz.Debug("SET COMMAND SEND {0}".format(post_fields))
+        self.melcloud_send_data_json(self.melcloud_urls["set_unit"], "{"+post_fields+"}", "SET")
 
     def melcloud_get_unit_info(self, unit):
         url = self.melcloud_urls["unit_info"] + "?id=" + str(unit['id']) + "&buildingID=" + str(unit['building_id'])
@@ -489,9 +515,9 @@ class BasePlugin:
             for level, pic in self.domoticz_levels["mode_pic"].items():
                 if level == setModeLevel:
                     setPicID = pic
-            Devices[self.list_switchs[0]["id"]+unit["idoffset"]].Update(nValue=switch_value,
-                                                                        sValue=setModeLevel,
-                                                                        Image=setPicID)
+                    Devices[self.list_switchs[0]["id"]+unit["idoffset"]].Update(nValue=switch_value,
+                                                                                sValue=setModeLevel,
+                                                                                Image=setPicID)
             for level, fan in self.domoticz_levels["fan"].items():
                 if fan == unit['set_fan']:
                     setDomFan = level
