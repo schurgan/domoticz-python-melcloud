d19ad65d0330454fab41526b0542917794b41687
schurgan <51407784+schurgan@users.noreply.github.com>
2025-12-26 13:40:34 +0100

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -29,10 +29,7 @@
 #        Usefull if you use your Mitsubishi remote
 # v0.1 : Initial release
 """
-<plugin key="MELCloud" version="0.9.0" name="MELCloud plugin"
-    author="gysmo schurgan dalonsic ChatGPT"
-    externallink="http://www.melcloud.com/"
-    externallink="https://github.com/schurgan/domoticz-BSR" >
+<plugin key="MELCloud" version="0.9.0" name="MELCloud plugin" author="gysmo schurgan dalonsic ChatGPT" wikilink="https://github.com/schurgan/domoticz-BSR" externallink="http://www.melcloud.com">
     <params>
         <param field="Username" label="Email" width="200px" required="true" />
         <param field="Password" label="Password" width="200px" required="true" password="true"/>
